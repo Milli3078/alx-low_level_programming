82dd4ce4077397e3c2d5f6451917df9ec96d8e32
Milli3078 <melaketsigea@gmail.com>
2023-03-16 13:47:15 -0700

    c lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio .h>
+#include <stdio.h>
 /**
  * main -Entry point
  * retun always 0(success)
  */
 int main(void)
 {
-	printf("'\ with proper grammar, but the outcome is a piece of art")
+	puts(" with proper grammar, but the outcome is a piece of art")
 		rturn 0;
 }
