99e04e22c58a3d63cdb6ad6c20959af00ca28e36
Milli3078 <melaketsigea@gmail.com>
2023-03-16 14:33:28 -0700

    c lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #iclude <stdio.h>
+
 /**
- * main - A program that prints aline with puts function
+ * main prints "Programming is like building a multilingual puzzle"
  * return: 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
