6622e6a1aaff26231e3287823d3f2661e0dd2807
Milli3078 <melaketsigea@gmail.com>
2023-03-17 14:33:55 -0700

    c-variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,13 @@
 /**
  * main - Prints numbers between 0 to 9 and letters between a a to f.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48: i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
