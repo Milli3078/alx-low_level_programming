44422cc49742f309ed69ee7569b4b7c1a50626be
Milli3078 <melaketsigea@gmail.com>
2023-03-17 15:16:33 -0700

    c-variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,19 @@
 /**
  * main - Prints the alphabet without q and e.
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = 97; i < 123; i++)
-{
-	if (i != 101 && i != 113)
-{
-	putchar(i);
+        int i;
 
- }
-}
-         putchar('\n');
-         return (0);
+        for (i = 97; i < 123; i++)
+	{
+	        if (i != 101 && i != 113)
+	        {
+		       	putchar(i);
+		}
+        }
+        putchar('\n');
+        return (0);
 }
